42244e4f9b1165ccb75c9e5ae408519e5589d783
danieltata2 <78004545+danieltata2@users.noreply.github.com>
2025-05-29 16:34:02 +0200

    Create Resultado.py

---
+++ Resultado.py
@@ -0,0 +1,6 @@
+import flet as ft
+
+def resultado_view():
+    return ft.Column([
+        ft.Text("INICIO DE SESIÓN EXITOSO", size=30, color="green")
+    ], alignment=ft.MainAxisAlignment.CENTER)
